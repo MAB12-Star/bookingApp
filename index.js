70fcb26bfe6a5ec3629f46dffde980cd3b4e4f42
Michael <michael.aron.blue@gmail.com>
2024-01-07 08:40:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.get('/', (req, res) => {
   res.render('home');
